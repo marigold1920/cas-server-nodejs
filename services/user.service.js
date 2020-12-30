0d9632fef840d1582d9c6c15b200a6bbf2418391
LeHuy <cwalklhuy@gmail.com>
2020-12-30 23:01:57 +0700

    Hốt tiền user controller

--- services/user.service.js
+++ services/user.service.js
@@ -55,7 +55,7 @@ exports.authenticateRequester = asyncHandler(async (req, res) => {
     if (!user || !(await bcrypt.compare(password, user.password)))
         throw 'Username or password is incorrect';
     const token = jwt.sign({ sub: user.id }, process.env.JWT_KEY, { expiresIn: '1d' });
-    
+
     res.status(200).json({ ...omitHash(user.get()), token });
 
 });
@@ -70,7 +70,7 @@ exports.authenticateDriver = asyncHandler(async (req, res) => {
     if (!user || !(await bcrypt.compare(password, user.password)))
         throw 'Username or password is incorrect';
     const token = jwt.sign({ sub: user.id }, process.env.JWT_KEY, { expiresIn: '1d' });
-    
+
     res.status(200).json({ ...omitHash(user.get()), token });
 });
 exports.authenticateAdmin = asyncHandler(async (req, res) => {
@@ -84,26 +84,118 @@ exports.authenticateAdmin = asyncHandler(async (req, res) => {
     if (!user || !(await bcrypt.compare(password, user.password)))
         throw 'Username or password is incorrect';
     const token = jwt.sign({ sub: user.id }, process.env.JWT_KEY, { expiresIn: '1d' });
-    
+
     res.status(200).json({ ...omitHash(user.get()), token });
 });
 exports.forgetPasswordRequester = asyncHandler(async (req, res) => {
 
+    try {
+        const result = await model.User.update(
+            { password: await bcrypt.hash(req.body.password, 10) },
+            {
+                where: {
+                    username: req.body.username,
+                    role_id: 1
+                }
+            }
+        )
+        res.status(200).json({ message: 'Update success' });
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
 exports.forgetPasswordDriver = asyncHandler(async (req, res) => {
-
+    try {
+        await model.User.update(
+            { password: await bcrypt.hash(req.body.password, 10) },
+            {
+                where: {
+                    username: req.body.username,
+                    role_id: 2
+                }
+            }
+        )
+        res.status(200).json({ message: 'Update success' });
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
 exports.forgetPasswordAdmin = asyncHandler(async (req, res) => {
+    try {
+        await model.User.update(
+            { password: await bcrypt.hash(req.body.password, 10) },
+            {
+                where: {
+                    username: req.body.username,
+                    role_id: 3
+                }
+            }
+        )
 
+        res.status(200).json({ message: 'Update success' });
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
 exports.checkExistedRequester = asyncHandler(async (req, res) => {
-
+    const username = req.query.username;
+    try {
+        const user = await model.User.findOne(
+            {
+                where: {
+                    username: username,
+                    role_id: 1
+                }
+            }
+        )
+        if (!user) {
+            res.status(404).json({message: 'User not found'})
+        } else {
+            res.status(200).json({username: username})
+        }
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
 exports.checkExistedDriver = asyncHandler(async (req, res) => {
-
+    const username = req.query.username;
+    try {
+        const user = await model.User.findOne(
+            {
+                where: {
+                    username: username,
+                    role_id: 2
+                }
+            }
+        )
+        if (!user) {
+            res.status(404).json({message: 'User not found'})
+        } else {
+            res.status(200).json({username: username})
+        }
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
 exports.checkExistedAdmin = asyncHandler(async (req, res) => {
-
+    const username = req.query.username;
+    try {
+        const user = await model.User.findOne(
+            {
+                where: {
+                    username: username,
+                    role_id: 3
+                }
+            }
+        )
+        if (!user) {
+            res.status(404).json({message: 'User not found'})
+        } else {
+            res.status(200).json({username: username})
+        }
+    } catch (err) {
+        res.status(500).json({ message: err });
+    }
 });
 
 
