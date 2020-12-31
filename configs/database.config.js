c1cf448ab983f930aab890ee6f994e029f65fff3
LeHuy <cwalklhuy@gmail.com>
2020-12-31 16:04:05 +0700

    Change link db

--- configs/database.config.js
+++ configs/database.config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("cas", "root", "12345678", {
-    host: "localhost",
+const sequelize = new Sequelize("cas", "admin", "12345679", {
+    host: "cas-capstone.cx6mpywgkrge.ap-southeast-1.rds.amazonaws.com",
     dialect: "mysql"
 });
 
